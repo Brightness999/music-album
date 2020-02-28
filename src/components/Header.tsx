1249bf48964b4e322d2fa6731554fe09e951c51f
Brightness999 <brightness0804@gmail.com>
2020-02-28 07:26:44 +0200

    fix(header): add search icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { Input } from "reactstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export function Header() {
     return (
@@ -9,7 +11,10 @@ export function Header() {
                 <NavLink to="home" activeClassName="active">Home</NavLink>
                 <NavLink to="genres" activeClassName="active">Genres</NavLink>
                 <NavLink to="all-releases" activeClassName="active">All releases</NavLink>
-                <Input placeholder="Search here..." id="iSearch"></Input>
+                <div className="d-flex align-items-center">
+                    <FontAwesomeIcon icon={faSearch} className="search-icon"/>
+                    <Input placeholder="Search here..." id="iSearch"></Input>
+                </div>
             </div>
             <div className="d-flex align-items-center">
                 <NavLink to="premium" activeClassName="active">Premium</NavLink>
