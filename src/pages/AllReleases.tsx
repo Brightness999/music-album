f80a80fceb04b20d5fd3018a22399e34e6cbc481
Brightness999 <brightness0804@gmail.com>
2020-02-28 07:43:00 +0200

    feat(all-releases): add sort button

--- src/pages/AllReleases.tsx
+++ src/pages/AllReleases.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThLarge, faThList } from '@fortawesome/free-solid-svg-icons';
 
 export default function AllReleases() {
     return (
         <div className="page">
-            <p className="page-title">All releases</p>
+            <div className="d-flex justify-content-between">
+                <p className="page-title">All releases</p>
+                <div className="d-flex align-items-center">
+                    <Button className="playback-control track-control"><FontAwesomeIcon icon={faThLarge} />&nbsp;&nbsp;Grid</Button>
+                    <Button className="playback-control track-control"><FontAwesomeIcon icon={faThList} />&nbsp;&nbsp;List</Button>
+                </div>
+            </div>
+            
         </div>
     );
 }
