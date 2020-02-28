c659c294003693322ba35e28410e3970607580f8
Brightness999 <brightness0804@gmail.com>
2020-02-28 07:11:53 +0200

    fix(playbar): width error

--- src/App.tsx
+++ src/App.tsx
@@ -33,8 +33,8 @@ export default function App() {
             <Home />
           </Route>
         </Switch>
-        <PlayBar></PlayBar>
       </div>
+      <PlayBar></PlayBar>
     </Router>
   );
 }
