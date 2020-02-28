a8194a18c70d7b0b88469ce4eb73f1c55a2de7a7
Brightness999 <brightness0804@gmail.com>
2020-02-28 07:20:42 +0200

    fix(header): link active

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { Input } from "reactstrap";
 
 export function Header() {
     return (
         <header className="d-flex align-items-center justify-content-between">
             <div className="d-flex align-items-center">
-                <Link to="home" className="active">Home</Link>
-                <Link to="genres">Genres</Link>
-                <Link to="all-releases">All releases</Link>
+                <NavLink to="home" activeClassName="active">Home</NavLink>
+                <NavLink to="genres" activeClassName="active">Genres</NavLink>
+                <NavLink to="all-releases" activeClassName="active">All releases</NavLink>
                 <Input placeholder="Search here..." id="iSearch"></Input>
             </div>
             <div className="d-flex align-items-center">
-                <Link to="premium">Premium</Link>
-                <Link to="account">Account</Link>
-                <Link to="contact">Contact</Link>
+                <NavLink to="premium" activeClassName="active">Premium</NavLink>
+                <NavLink to="account" activeClassName="active">Account</NavLink>
+                <NavLink to="contact" activeClassName="active">Contact</NavLink>
             </div>
         </header>
     );
