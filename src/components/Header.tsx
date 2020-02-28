c1a41490d4a65b5e3eb150aed48c1da84b7c9073
Brightness999 <brightness0804@gmail.com>
2020-02-28 07:15:55 +0200

    fix(header): add premium, account, contact link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,18 @@ import { Input } from "reactstrap";
 
 export function Header() {
     return (
-        <header className="d-flex align-items-center">
-            <Link to="home" className="active">Home</Link>
-            <Link to="genres">Genres</Link>
-            <Link to="all-releases">All releases</Link>
-            <Input placeholder="Search here..." id="iSearch"></Input>
+        <header className="d-flex align-items-center justify-content-between">
+            <div className="d-flex align-items-center">
+                <Link to="home" className="active">Home</Link>
+                <Link to="genres">Genres</Link>
+                <Link to="all-releases">All releases</Link>
+                <Input placeholder="Search here..." id="iSearch"></Input>
+            </div>
+            <div className="d-flex align-items-center">
+                <Link to="premium">Premium</Link>
+                <Link to="account">Account</Link>
+                <Link to="contact">Contact</Link>
+            </div>
         </header>
     );
 }
