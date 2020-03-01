2bb34c130c0ddc318d0853705fc16c6f45bed157
Brightness999 <brightness0804@gmail.com>
2020-03-01 23:53:35 +0200

    feat: album select event

--- src/components/LargeAlbumItem.tsx
+++ src/components/LargeAlbumItem.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
-import {Album, AlbumResponse, Track} from '../models';
+import {NavLink} from 'react-router-dom';
+import {Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle} from 'reactstrap';
+import {Album, AlbumResponse} from '../models';
 import axios, {AxiosResponse} from "axios";
 import {useDispatch} from "react-redux";
-import {setCurrentTrack, setPlayList} from "../store";
+import {setPlayList} from "../store";
+import {faPlay} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 interface IProps {
     album: Album;
@@ -14,19 +16,26 @@ export default function LargeAlbumItem(props: IProps) {
     const dispatch = useDispatch();
     return (
         <Card>
-            <CardImg top
-                     src={`/uploads/albums/${props.album.location}/thumb/${props.album.slug}.jpg`}
-                     alt="album"
-                     onClick={() => {
-                         const fetchData = async () => {
-                             const result: AxiosResponse<AlbumResponse> = await axios(`/api/album/${props.album.slug}`);
-                             const play_list = result.data.album.tracks.map(track => track.slug);
-                             dispatch(setPlayList(play_list));
-                         };
-                         fetchData();
+            <div
+                onClick={() => {
+                    const fetchData = async () => {
+                        const result: AxiosResponse<AlbumResponse> = await axios(`/api/album/${props.album.slug}`);
+                        const play_list = result.data.album.tracks.map(track => track.slug);
+                        dispatch(setPlayList(play_list));
+                    };
+                    fetchData();
 
-                     }}
-            />
+                }}
+                className="position-relative d-flex"
+            >
+                <CardImg top
+                         src={`/uploads/albums/${props.album.location}/thumb/${props.album.slug}.jpg`}
+                         alt="album"
+                />
+                <div className="album-cover d-flex justify-content-center align-items-center">
+                    <FontAwesomeIcon icon={ faPlay }/>
+                </div>
+            </div>
             <CardBody>
                 <CardTitle><NavLink to={"/album/"+props.album.slug}>{props.album.title}</NavLink></CardTitle>
                 <CardSubtitle>{props.album.catalog}</CardSubtitle>
--- src/components/TopAlbumItem.tsx
+++ src/components/TopAlbumItem.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 
-import {Album} from "../models";
+import {Album, AlbumResponse} from "../models";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlay} from "@fortawesome/free-solid-svg-icons";
+import axios, {AxiosResponse} from "axios";
+import {setPlayList} from "../store";
+import {useDispatch} from "react-redux";
 
 interface IProps {
     album: Album;
 }
 
 export default function TopAlbumItem(props: IProps) {
+    const dispatch = useDispatch();
     return (
         <div className="top-album-item pt-1 pb-1 pl-3">
-            <div className="img-wrapper">
+            <div
+                onClick={() => {
+                    const fetchData = async () => {
+                        const result: AxiosResponse<AlbumResponse> = await axios(`/api/album/${props.album.slug}`);
+                        const play_list = result.data.album.tracks.map(track => track.slug);
+                        dispatch(setPlayList(play_list));
+                    };
+                    fetchData();
+
+                }}
+                className="img-wrapper position-relative d-flex"
+            >
                 <img src={`/uploads/albums/${props.album.location}/thumb/${props.album.slug}.jpg`} alt="album"/>
+                <div className="w-100 h-100 d-flex justify-content-center align-items-center album-cover">
+                    <FontAwesomeIcon icon={ faPlay }/>
+                </div>
             </div>
             <div className="content ml-2">
                 <div className="title">
