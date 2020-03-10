4491b2ed4d96a7a4d7d26859c7cf6cccc9553794
Brightness999 <brightness0804@gmail.com>
2020-03-10 20:43:12 +0200

    feat: contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
 export default function ContactPage() {
-    return <div>Contact Page(not implemented yet)</div>;
+    return <div className="page">
+        <h1 className="pb-2">Contact Information</h1>
+        <h2 className="pl-3 pb-2">Customer Support</h2>
+        <p className="pl-3 pb-2">For help or questions about your account, please contact us.</p>
+        <h2 className="pl-3 pb-2">Contact Us</h2>
+        <p className="pl-3">house-language.com</p>
+        <p className="pl-3">Artem Setnik</p>
+        <p className="pl-3">captainsuper328@gmail.com</p>
+    </div>;
 }
