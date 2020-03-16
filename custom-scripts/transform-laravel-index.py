f9c91bf7230c705b0f6430f9fae06548cdb765a6
Brightness999 <brightness0804@gmail.com>
2020-03-16 12:16:16 +0200

    feat: script for laravel deploy

---
+++ custom-scripts/transform-laravel-index.py
@@ -0,0 +1,14 @@
+from os.path import exists
+
+if __name__ == '__main__':
+    f_path = './build/index.html'
+    if not exists(f_path):
+        print(f'index.html not found in build directory. Exiting...')
+        exit()
+    f = open(f_path, 'r')
+    data = f.read().replace('href="/static/', 'href="/assets/spa/')
+    f.close()
+    f = open(f_path, 'w')
+    f.write(data)
+    f.close()
+    print(f'index.html is ready for laravel deploy.')
