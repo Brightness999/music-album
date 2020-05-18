4ea8bd9c82f59980a363f0052f254c17eaec749c
Brightness999 <brightness0804@gmail.com>
2020-05-18 10:05:05 +0300

    feat: add meta tags spaces in index file

--- src/components/AlbumPaginationItem.tsx
+++ src/components/AlbumPaginationItem.tsx
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { PaginationItem, PaginationLink } from 'reactstrap';
 import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { selectCurrentPage, selectPageCount } from '../redux/selectors';
 import { composePageLink } from '../common';
 import history from '../history';
 
 interface Props {
-    pageNumber: number;
+  pageNumber: number;
 }
 
 export default function AlbumPaginationItem(props: Props) {
-    const currentPage = useSelector(selectCurrentPage);
-    const pageCount = useSelector(selectPageCount);
-    const loc = history.location;
-    const pathName = loc.pathname;
-    const newPath = composePageLink(pathName, pageCount, props.pageNumber);
-    return (<PaginationItem active={props.pageNumber === currentPage}>
-        <PaginationLink href={newPath}>{props.pageNumber+1}</PaginationLink>
-    </PaginationItem>);
+  const currentPage = useSelector(selectCurrentPage);
+  const pageCount = useSelector(selectPageCount);
+  const loc = history.location;
+  const pathName = loc.pathname;
+  const newPath = composePageLink(pathName, pageCount, props.pageNumber);
+  return (<PaginationItem active={props.pageNumber === currentPage}>
+    <PaginationLink><NavLink to={newPath}>{props.pageNumber + 1}</NavLink></PaginationLink>
+  </PaginationItem>);
 }
